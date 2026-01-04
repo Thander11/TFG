ee04c67da8f7212003201abcf2d76f60c3d50c3d
Thander <80818141+Thander11@users.noreply.github.com>
2026-01-04 13:20:16 +0100

    Busqueda limpia y estilo
    Ahora la aplicación ya busca los dispostivios bluetooth eliminando los desconocidos de la lista, haciendo que todo sea mucho mas limpio. Además se ha cambiado el nombre de la app de movil para que la primera letra sea con mayusculas y se le ha puesto un icono provisional

--- Sniffer/sniffer/lib/main.dart
+++ Sniffer/sniffer/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,20 +14,43 @@ class MyApp extends StatelessWidget {
         body: StreamBuilder<List<ScanResult>>(
           stream: FlutterBluePlus.scanResults,
           builder: (c, snapshot) {
+            // 1. Filtramos los resultados: 
+            // Que el nombre no esté vacío y (opcionalmente) que contenga "RN" o "Microchip"
+            final filteredResults = (snapshot.data ?? [])
+                .where((r) => r.device.platformName.isNotEmpty) 
+                .toList();
+
             return ListView(
-              children: (snapshot.data ?? [])
+              children: filteredResults
                   .map((r) => ListTile(
-                        title: Text(r.device.platformName.isEmpty ? "Desconocido" : r.device.platformName),
+                        title: Text(r.device.platformName),
                         subtitle: Text(r.device.remoteId.toString()),
+                        leading: const Icon(Icons.bluetooth_connected, color: Colors.blue),
+                        trailing: Text("${r.rssi} dBm"), // Esto te dice la distancia
                         onTap: () => print("Conectando a: ${r.device.remoteId}"),
                       ))
                   .toList(),
             );
           },
         ),
-        floatingActionButton: FloatingActionButton(
+                floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.search),
-          onPressed: () => FlutterBluePlus.startScan(timeout: const Duration(seconds: 4)),
+          onPressed: () async {
+            // 1. Pedir permisos antes de escanear
+            Map<Permission, PermissionStatus> statuses = await [
+              Permission.bluetoothScan,
+              Permission.bluetoothConnect,
+              Permission.location,
+            ].request();
+
+            // 2. Si están concedidos, empezar escaneo
+            if (statuses[Permission.bluetoothScan]!.isGranted &&
+                statuses[Permission.location]!.isGranted) {
+              FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
+            } else {
+              print("Permisos denegados por el usuario");
+            }
+          },
         ),
       ),
     );
